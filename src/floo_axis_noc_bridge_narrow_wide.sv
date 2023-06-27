42fa8e95ce7ba18d6bba57416aaafe57b82d9584
Yannick Baumann <baumanny@student.ethz.ch>
2023-06-27 17:46:31 +0200

    solved two bugs in the data_link_layer and introduced support for wide/narrow channel selection. (currently only in the floo_axis_noc_bridge_narrow_wide version, virtual-channel version will follow)

--- src/floo_axis_noc_bridge_narrow_wide.sv
+++ src/floo_axis_noc_bridge_narrow_wide.sv
@@ -1,6 +1,6 @@
 // Yannick Baumann <baumanny@student.ethz.ch>
 `include "common_cells/assertions.svh"
-// TODO: currently this is only a template...
+`include "common_cells/registers.svh"
 
 module floo_axis_noc_bridge_narrow_wide
 #(
@@ -8,12 +8,9 @@ module floo_axis_noc_bridge_narrow_wide
   parameter  bit  ignore_assert     = 1'b0,
   parameter  type narrow_rsp_flit_t = logic,
   parameter  type narrow_req_flit_t = logic,
+  parameter  type wide_flit_t       = logic,
   parameter  type axis_req_t        = logic,
-  parameter  type axis_rsp_t        = logic,
-  parameter  int  numNocChanPerDir  = 2,
-
-  localparam int unsigned IdxWidth = unsigned'($clog2(numNocChanPerDir)),
-  localparam type         idx_t    = logic [IdxWidth-1:0]
+  parameter  type axis_rsp_t        = logic
 ) (
   // global signals
   input  logic      clk_i,
@@ -25,9 +22,9 @@ module floo_axis_noc_bridge_narrow_wide
     // flits to be received
   input  narrow_req_flit_t narrow_req_i,
   input  narrow_rsp_flit_t narrow_rsp_i,
-    // wide channels => TODO: use the wide channels
-  input  wide_i,
-  output wide_o,
+    // wide channels
+  input  wide_flit_t       wide_i,
+  output wide_flit_t       wide_o,
   // AXIS channels
     // AXIS outgoing data
   output axis_req_t axis_out_req_o,
@@ -39,40 +36,51 @@ module floo_axis_noc_bridge_narrow_wide
 
   import noc_bridge_narrow_wide_pkg::*;
 
-  idx_t selected_index;
-
   typedef struct packed {
-    channel_hdr_e hdr;
     logic [NarrowFlitDataSize-1:0] flit_data;
+    channel_hdr_e hdr;
+  } narrow_axis_data_t;
+
+    typedef struct packed {
+    logic [WideFlitDataSize-1:0] flit_data;
+    channel_hdr_e hdr;
+  } wide_axis_data_t;
+
+    typedef struct packed {
+    logic [NarrowFlitDataSize-1:0] flit_data;
+    channel_hdr_e hdr;
   } axis_data_t;
 
-  axis_data_t axis_out_payload, axis_in_payload;
-  axis_data_t axis_out_data_reg_out;
+  selected_channel_type_e selChanType_q, selChanType_d;
+
+  narrow_axis_data_t arbiter_out_payload;
+  wide_axis_data_t axis_out_payload, axis_in_payload;
+  wide_axis_data_t axis_out_data_reg_out;
   logic axis_out_ready, axis_out_valid;
-  localparam int payloadSize = $bits(axis_data_t);
+  logic arbiter_ready_in, arbiter_valid_out;
 
   // the axis data payload also contains the header bit which is why the flit data width is one bit smaller than the payload
-  logic [payloadSize-2:0] narrow_req_i_data, narrow_rsp_i_data;
+  narrow_axis_data_t narrow_req_i_data, narrow_rsp_i_data;
 
   ////////////////////////////////////////////////
   //  CONNECT INCOMING FLITS WITH THE AXIS_OUT  //
   ////////////////////////////////////////////////
 
   // Assignment required to match the data width of the two channels (rr_arb_tree needs equi-size signals)
-  assign narrow_req_i_data = narrow_req_i.data;
-  assign narrow_rsp_i_data = narrow_rsp_i.data;
+  assign narrow_req_i_data = { narrow_req_i.data, narrow_request  };
+  assign narrow_rsp_i_data = { narrow_rsp_i.data, narrow_response };
 
   rr_arb_tree #(
-    .NumIn      ( numNocChanPerDir           ),
-    .DataWidth  ( payloadSize - 1            ),
-    .ExtPrio    ( 1'b0                       ),
-    .AxiVldRdy  ( 1'b1                       ),
-    .LockIn     ( 1'b0                       )
+    .NumIn      ( 2                  ),
+    .DataType   ( narrow_axis_data_t ),
+    .ExtPrio    ( 1'b0               ),
+    .AxiVldRdy  ( 1'b1               ),
+    .LockIn     ( 1'b0               )
   ) i_rr_arb_tree (
-    .clk_i      ( clk_i                      ),
-    .rst_ni     ( rst_ni                     ),
+    .clk_i      ( clk_i                                    ),
+    .rst_ni     ( rst_ni                                   ),
     /// Clears the arbiter state. Only used if `ExtPrio` is `1'b0` or `LockIn` is `1'b1`.
-    .flush_i    ( 1'b0                       ),
+    .flush_i    ( 1'b0                                     ),
     /// Input requests arbitration.
     .req_i      ( {narrow_req_i.valid, narrow_rsp_i.valid} ),
     /* verilator lint_off UNOPTFLAT */
@@ -82,19 +90,44 @@ module floo_axis_noc_bridge_narrow_wide
     /// Input data for arbitration.
     .data_i     ( {narrow_req_i_data, narrow_rsp_i_data}   ),
     /// Output request is valid.
-    .req_o      ( axis_out_valid             ),
+    .req_o      ( arbiter_valid_out                        ),
     /// Output request is granted.
-    .gnt_i      ( axis_out_ready             ),
+    .gnt_i      ( arbiter_ready_in                         ),
     /// Output data.
-    .data_o     ( axis_out_payload.flit_data ),
-    /// Index from which input the data came from.
-    .idx_o      ( selected_index             )
+    .data_o     ( {arbiter_out_payload.flit_data, arbiter_out_payload.hdr} )
   );
 
-  assign axis_out_payload.hdr = channel_hdr_e'(selected_index);
+  always_comb begin
+    selChanType_d = selChanType_q;
+    arbiter_ready_in = 0;
+    wide_o.ready = 0;
+    unique case (selChanType_q)
+      narrowChan : begin
+        axis_out_payload.hdr = arbiter_out_payload.hdr;
+        axis_out_payload.flit_data = arbiter_out_payload.flit_data;
+        axis_out_valid   = arbiter_valid_out;
+        arbiter_ready_in = axis_out_ready;
+        if (wide_i.valid & !arbiter_valid_out) begin
+          selChanType_d = wideChan;
+        end
+      end
+      wideChan : begin
+        axis_out_payload.hdr = wide_channel;
+        axis_out_payload.flit_data = wide_i.data;
+        axis_out_valid = wide_i.valid;
+        wide_o.ready   = axis_out_ready;
+        if ((!wide_i.valid | wide_o.ready) & arbiter_valid_out) begin
+          selChanType_d = narrowChan;
+        end
+      end
+      default : /* default */;
+    endcase
+  end
+
+  `FF(selChanType_q, selChanType_d, narrowChan);
 
   stream_fifo #(
-    .DATA_WIDTH ( payloadSize           ),
+    .T          ( wide_axis_data_t      ),
     .DEPTH      ( 2                     )
   ) i_axis_out_reg (
     .clk_i      ( clk_i                 ),
@@ -111,7 +144,7 @@ module floo_axis_noc_bridge_narrow_wide
   );
 
   assign axis_out_req_o.t.data = axis_out_data_reg_out;
-  assign axis_out_req_o.t.strb = '1;
+  assign axis_out_req_o.t.strb = (axis_out_data_reg_out.hdr == wide_channel) ? WideStrobe : NarrowStrobe;
   assign axis_out_req_o.t.keep = '0;
   assign axis_out_req_o.t.last = '0;
   assign axis_out_req_o.t.id   = '0;
@@ -122,12 +155,14 @@ module floo_axis_noc_bridge_narrow_wide
   //  CONNECT AXIS_IN WITH THE OUTGOING FLITS  //
   ///////////////////////////////////////////////
 
-  assign axis_in_payload      = axis_data_t'(axis_in_req_i.t.data);
-  assign axis_in_rsp_o.tready = (narrow_req_i.ready & narrow_req_o.valid) || (narrow_rsp_i.ready & narrow_rsp_o.valid);
-  assign narrow_req_o.valid          = (axis_in_payload.hdr == request) ? axis_in_req_i.tvalid : 0;
-  assign narrow_rsp_o.valid          = (axis_in_payload.hdr == response) ? axis_in_req_i.tvalid : 0;
-  assign narrow_req_o.data           = axis_in_payload.flit_data;
-  assign narrow_rsp_o.data           = axis_in_payload.flit_data;
+  assign axis_in_payload      = wide_axis_data_t'(axis_in_req_i.t.data);
+  assign axis_in_rsp_o.tready = (narrow_req_i.ready & narrow_req_o.valid) || (narrow_rsp_i.ready & narrow_rsp_o.valid) || (wide_i.ready & wide_o.valid);
+  assign narrow_req_o.valid   = (axis_in_payload.hdr == narrow_request) ? axis_in_req_i.tvalid : 0;
+  assign narrow_rsp_o.valid   = (axis_in_payload.hdr == narrow_response) ? axis_in_req_i.tvalid : 0;
+  assign wide_o.valid         = (axis_in_payload.hdr == wide_channel) ? axis_in_req_i.tvalid : 0;
+  assign narrow_req_o.data    = axis_in_payload.flit_data;
+  assign narrow_rsp_o.data    = axis_in_payload.flit_data;
+  assign wide_o.data          = axis_in_payload.flit_data;
 
   // FOR THE TIME BEING THE SIGNALS BELOW ARE IGNORED...
   // assign ??? = axis_in_req_i.t.strb;
--- src/noc_bridge_narrow_wide_pkg.sv
+++ src/noc_bridge_narrow_wide_pkg.sv
@@ -9,7 +9,7 @@ package noc_bridge_narrow_wide_pkg;
 
   // With this parameter you can set the maximal credit count for the NoC bridge. If set to 0, the NoC-bridge without
   // virtualization will be used instead.
-  localparam int NumCred_NocBridge = 8;
+  localparam int NumCred_NocBridge = 0;
 
   ///////////////////////////////////////////
   // DEPENDANT PARAMETERS - DO NOT CHANGE  //
@@ -25,14 +25,36 @@ package noc_bridge_narrow_wide_pkg;
   // the minimal flit-data-size requirement corresponds to the larger of the two channels, exclusive the handshake signals.
   localparam int NarrowFlitDataSize = serial_link_pkg::find_max_channel(NarrowFlitTypes)-2;
 
-  typedef logic [NarrowFlitDataSize-1:0] flit_data_t;
-  typedef logic [NarrowReqFlitSize-3:0] flit_req_data_t;
-  typedef logic [NarrowRspFlitSize-3:0] flit_rsp_data_t;
+  typedef logic [NarrowFlitDataSize-1:0] narrow_flit_data_t;
+  typedef logic [NarrowReqFlitSize-3:0] narrow_flit_req_data_t;
+  typedef logic [NarrowRspFlitSize-3:0] narrow_flit_rsp_data_t;
+
+
+  localparam int WideFlitSize  = $bits(wide_flit_t);
+  localparam int WideFlitDataSize = WideFlitSize-2;
+
+  typedef logic [WideFlitDataSize-1:0] wide_flit_data_t;
+
+  typedef enum logic [1:0] {
+    narrow_response = 'd0,
+    narrow_request  = 'd1,
+    wide_channel    = 'd2
+  } channel_hdr_e;
 
   typedef enum logic [0:0] {
-    response  = 'd0,
-    request   = 'd1
-  } channel_hdr_e;
+    narrowChan = 'd0,
+    wideChan   = 'd1
+  } selected_channel_type_e;
+
+  localparam int strobeSize   = (($bits(wide_flit_data_t)+$bits(channel_hdr_e)+7)/8);
+  localparam int narrowStrobe = (($bits(narrow_flit_data_t)+$bits(channel_hdr_e)+7)/8);
+
+  typedef logic [strobeSize-1:0] strb_noc_t;
+
+  localparam strb_noc_t NarrowStrobe = {narrowStrobe{1'b1}};
+  localparam strb_noc_t WideStrobe   = {strobeSize{1'b1}};
+
+  localparam int WideChannelHdr = $bits(channel_hdr_e);
 
   typedef logic [$clog2(NumCred_NocBridge+1)-1:0] bridge_credit_t;
 
@@ -46,16 +68,24 @@ package noc_bridge_narrow_wide_pkg;
   // Data bits utilized in the AXIS interface
   typedef struct packed {
     channel_hdr_e data_hdr;
-    flit_data_t data;
+    wide_flit_data_t data;
   } data_bits_t;
 
   // packet type used in the virtual-channel noc_bridge
   typedef struct packed {
     channel_hdr_e data_hdr;
-    flit_data_t data;
+    wide_flit_data_t data;
     logic data_validity;
     channel_hdr_e credits_hdr;
     bridge_credit_t credits;
   } axis_packet_t;
+
+  typedef struct packed {
+    channel_hdr_e data_hdr;
+    narrow_flit_data_t data;
+    logic data_validity;
+    channel_hdr_e credits_hdr;
+    bridge_credit_t credits;
+  } narrow_axis_packet_t;
 
 endpackage : noc_bridge_narrow_wide_pkg
