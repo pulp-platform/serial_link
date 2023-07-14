94b5b5da9ebac39c7d7321a86f6322755cc77a0c
Yannick Baumann <baumanny@student.ethz.ch>
2023-07-14 15:43:40 +0200

    few adjustments on the synthesis setup

--- gf22/synopsys/scripts/synth.tcl
+++ gf22/synopsys/scripts/synth.tcl
@@ -7,19 +7,17 @@
 
 # synthesize the design => make sure to match the name of the library script!!!
 source [file dirname [info script]]/seriallink_lib.tcl
-# # TODO: or is it this path?
-# source [file dirname [info script]]/../seriallink_lib.tcl
 set_host_options -max_cores 16
 
-# (if clause removed): TCK used in the constraints section (time in ps) => set the clock frequency
-set TCK 1000
-# original: if {![info exists TIMESTAMP      ]} {set TIMESTAMP       nulltimestamp}
-set TIMESTAMP [date]
+# TCK used in the constraints section (time in ps) => set the clock frequency
+if {![info exists TCK               ]} {set TCK                  1000           }
+if {![info exists TIMESTAMP         ]} {set TIMESTAMP  [exec date | tr " " "_"] }
 
 # set the name of the design => please give your design a reasonable name
 set DESIGN serial_link_narrow_wide
-set OUTNAME ${DESIGN}_${TIMESTAMP}
+set OUTNAME ${DESIGN}-clk_is_${TCK}ps-${TIMESTAMP}
 set REPDIR $SYNDIR/reports/$OUTNAME
+
 # set the constraints => select the input delay (ID), output delay (OD) and the load (LOAD)
 set ID 100
 set OD 100
@@ -42,39 +40,45 @@ set_app_var write_sdc_output_net_resistance         false
 ##   LOAD DESIGN   ##
 #####################
 
-# Set libraries
-dz_set_pvt [list \
-                GF22FDX_SC8T_104CPP_BASE_CSC20SL_TT_0P80V_0P00V_0P00V_0P00V_25C\
-                GF22FDX_SC8T_104CPP_BASE_CSC24SL_TT_0P80V_0P00V_0P00V_0P00V_25C\
-                GF22FDX_SC8T_104CPP_BASE_CSC28SL_TT_0P80V_0P00V_0P00V_0P00V_25C\
-                GF22FDX_SC8T_104CPP_BASE_CSC20L_TT_0P80V_0P00V_0P00V_0P00V_25C \
-                GF22FDX_SC8T_104CPP_BASE_CSC24L_TT_0P80V_0P00V_0P00V_0P00V_25C \
-                GF22FDX_SC8T_104CPP_BASE_CSC28L_TT_0P80V_0P00V_0P00V_0P00V_25C ]
+redirect -tee -file $REPDIR/synth_loadDesign.log {
+    # Set libraries
+    dz_set_pvt [list \
+                    GF22FDX_SC8T_104CPP_BASE_CSC20SL_TT_0P80V_0P00V_0P00V_0P00V_25C\
+                    GF22FDX_SC8T_104CPP_BASE_CSC24SL_TT_0P80V_0P00V_0P00V_0P00V_25C\
+                    GF22FDX_SC8T_104CPP_BASE_CSC28SL_TT_0P80V_0P00V_0P00V_0P00V_25C\
+                    GF22FDX_SC8T_104CPP_BASE_CSC20L_TT_0P80V_0P00V_0P00V_0P00V_25C \
+                    GF22FDX_SC8T_104CPP_BASE_CSC24L_TT_0P80V_0P00V_0P00V_0P00V_25C \
+                    GF22FDX_SC8T_104CPP_BASE_CSC28L_TT_0P80V_0P00V_0P00V_0P00V_25C ]
 
-# # TODO: Are these lines required?
-# # Load the memory libraries
-# foreach cut $MEMCUTS {
-#   lappend link_library ${cut}_104cpp_TT_0P800V_0P000V_0P000V_025C.db
-# }
+    # # TODO: Are these lines required?
+    # # Load the memory libraries
+    # foreach cut $MEMCUTS {
+    #   lappend link_library ${cut}_104cpp_TT_0P800V_0P000V_0P000V_025C.db
+    # }
 
-# Load the design
-read_file -format ddc $SYNDIR/DDC/${DESIGN}.ddc
+    # Load the design
+    read_file -format ddc $SYNDIR/DDC/${OUTNAME}.ddc
 
-# Link the design
-link
+    # Link the design
+    link
+}
 
 #####################
-##   CONSTRAINTS   ## => add here your constraints
+##   CONSTRAINTS   ## => add your constraints here
 #####################
 
-# Read constraints
-redirect -tee -file $REPDIR/constraints_${TCK}ps.rpt {
-    # set the constraints to the clock of the top-mesh
+# Read constraints (to print out in the synth_constraints file, put an echo of the command before the actual command)
+redirect -tee -file $REPDIR/synth_constraints.rpt {
+    echo "create_clock clk_i -period ${TCK}"
     create_clock clk_i -period ${TCK}
 
+    echo "set_input_delay ${ID} -clock clk_i [remove_from_collection [all_inputs] clk_i]"
     set_input_delay ${ID} -clock clk_i [remove_from_collection [all_inputs] clk_i]
+    echo "set_output_delay ${OD} -clock clk_i [all_outputs]"
     set_output_delay ${OD} -clock clk_i [all_outputs]
+    echo "set_driving_cell -no_design_rule -lib_cell SC8T_BUFX6_CSC20L -pin Z -from_pin A [all_inputs]"
     set_driving_cell -no_design_rule -lib_cell SC8T_BUFX6_CSC20L -pin Z -from_pin A [all_inputs]
+    echo "set_load ${LOAD} [all_outputs]"
     set_load ${LOAD} [all_outputs]
 
     # set_ideal_network : no delay, ...
@@ -101,12 +105,11 @@ redirect -tee -file $REPDIR/constraints_${TCK}ps.rpt {
     # # set_dont_touch_network [get_ports rst_ni]
 }
 
-
 ######################
 ##   COMPILE CORE   ##
 ######################
 
-redirect -tee -file $REPDIR/compile_step1_${TCK}ps.rpt {
+redirect -tee -file $REPDIR/synth_compile.log {
     # Compile
     compile_ultra -no_autoungroup
 }
@@ -115,7 +118,7 @@ redirect -tee -file $REPDIR/compile_step1_${TCK}ps.rpt {
 #  ##   INCREMENTAL COMPILE   ##
 #  #############################
 #
-#  redirect -tee -file $REPDIR/compile_step2_${TCK}ps.rpt {
+#  redirect -tee -file $REPDIR/synth_compile_incr.rpt {
 #
 #    # Do an incremental compile, independently on slack
 #    compile_ultra -no_autoungroup -incremental
@@ -128,15 +131,15 @@ redirect -tee -file $REPDIR/compile_step1_${TCK}ps.rpt {
 ###############
 
 # Absolute critical path
-report_timing -nosplit > $REPDIR/timing_${TCK}ps.rpt
-report_area -hierarchy -nosplit > $REPDIR/area_${TCK}ps.rpt
-report_resource -hierarchy -nosplit > $REPDIR/resources_${TCK}ps.rpt
-report_register -nosplit > $REPDIR/registers_${TCK}ps.rpt
+report_timing -nosplit > $REPDIR/synth_timing_report.rpt
+report_area -hierarchy -nosplit > $REPDIR/synth_area_report.rpt
+report_resource -hierarchy -nosplit > $REPDIR/synth_resources_report.rpt
+report_register -nosplit > $REPDIR/synth_registers_report.rpt
 # Ciao!
 # exit
 
 # suggested by the VLSI_1 ex6 to verify whether the run was successful or not => uncomment if needed
-check_design > $REPDIR/checkDesign_${TCK}ps.log
+check_design > $REPDIR/synth_checkDesign.log
 
 # ------------------------------------------------------------------------------
 # Write Out Data
@@ -147,6 +150,6 @@ change_names -rule verilog -hierarchy
 # Write Verilog netlist.
 sh mkdir $REPDIR/netlist
 write -hierarchy -format verilog -output $REPDIR/netlist/${DESIGN}.v
-link > $REPDIR/link_command.txt
-# sh cp /home/sem22h28/minoc/gf22/synopsys/reports/bsg_manycore_add_router/elaborate.rpt $REPDIR/elaborate.rpt
-echo "$OUTNAME: results from week ?, seems to be valid => constraints ???" >> $SYNDIR/reports/versionLog.log
+exec cp $REPDIR/netlist/${DESIGN}.v $SYNDIR/netlists/${OUTNAME}.v
+link > $REPDIR/synth_final_link_command.log
+echo "$OUTNAME: No comment" >> $SYNDIR/reports/versionLog.log
